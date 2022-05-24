71cb2de26b323ee5de13c229fd258bf63ac6c0af
Dark Radio 314 <jmlev@ucdavis.edu>
2022-05-23 17:10:39 -0700

    DAQ: updates some plot colors

--- daqAnalysisAndExperiments/antennaEffectOnThermalNoise_5.23.22/getRigolDataSaveAndPlot.py
+++ daqAnalysisAndExperiments/antennaEffectOnThermalNoise_5.23.22/getRigolDataSaveAndPlot.py
@@ -17,7 +17,7 @@ ADDRESS = 'TCPIP0::169.254.158.45::INSTR'
 startFreq = 800 * 10**6 #Hz
 stopFreq = 1200 * 10**6 #Hz
 nPoints = 10000 
-freqArr = np.linspace(startFreq, stopFreq, nPoints)
+freqArr = np.linspace(startFreq, stopFreq, nPoints)/1e6
 
 RM = visa.ResourceManager()
 INST = RM.open_resource(ADDRESS)
@@ -37,30 +37,35 @@ if 0:
     print('got it')
     print()
 
-    np.save('littleVivaldiCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn', specArr)
+    np.save('50OhmTermCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn', specArr)
 
 
 littleDipole = np.load('littleDipoleCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn.npy')
 yagi = np.load('yagiCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn.npy')
 vivaldi = np.load('littleVivaldiCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn.npy')
+term = np.load('50OhmTermCenterOfRoom_67dBGain_800-1200MHz_100Avg_100kRBW_rfPreOn.npy')
 
 yagiMedian = [np.median(x) for x in np.reshape(yagi, (-1,10))] 
 dipoleMedian = [np.median(x) for x in np.reshape(littleDipole, (-1,10))] 
 vivaldiMedian = [np.median(x) for x in np.reshape(vivaldi, (-1,10))] 
+termMedian = [np.median(x) for x in np.reshape(term, (-1,10))] 
+
 
 plt.figure()
-plt.plot(freqArr, yagi, label = 'Yagi')
-plt.plot(freqArr, littleDipole, label = 'Dipole', alpha = .5)
-plt.plot(freqArr, vivaldi, label = 'Vivaldi', alpha = .5)
+plt.plot(freqArr, yagi, 'b',  label = 'Yagi')
+plt.plot(freqArr, littleDipole, 'g', label = 'Dipole', alpha = 1)
+plt.plot(freqArr, vivaldi, 'r', label = 'Vivaldi', alpha = .6)
+plt.plot(freqArr, term, 'c', label = 'Term', alpha = .5)
 plt.xlabel('Freq. (MHz)')
 plt.ylabel('Power (dBm)')
 plt.title('67dB amp chain.')
 plt.legend()
 
 plt.figure()
-plt.plot(freqArr[::10], yagiMedian, label = 'Yagi')
-plt.plot(freqArr[::10], dipoleMedian, label = 'Dipole', alpha = .5)
-plt.plot(freqArr[::10], vivaldiMedian, label = 'Vivaldi', alpha = .5)
+plt.plot(freqArr[::10], yagiMedian, 'b', label = 'Yagi')
+plt.plot(freqArr[::10], dipoleMedian, 'g', label = 'Dipole', alpha = .7)
+plt.plot(freqArr[::10], vivaldiMedian, 'r', label = 'Vivaldi', alpha = .6)
+plt.plot(freqArr[::10], termMedian, 'y', label = 'Term', alpha = .5)
 plt.xlabel('Freq. (MHz)')
 plt.ylabel('Power (dBm)')
 plt.title('67dB amp chain. Median')
