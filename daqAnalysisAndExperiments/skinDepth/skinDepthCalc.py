ebfd0029282b9cd87f4efbc708b8cca231f219cd
josephmlev <joseph@campus-114-026.ucdavis.edu>
2022-07-08 11:33:43 -0700

    updates skin depth calculator

--- daqAnalysisAndExperiments/skinDepth/skinDepthCalc.py
+++ daqAnalysisAndExperiments/skinDepth/skinDepthCalc.py
@@ -11,18 +11,23 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
     
-def skin(s):
-    return (np.pi * 10e6 * 12.57e-7 * s)**(-1/2)
+def skin(s, freq):
+    return (np.pi * freq * 12.57e-7 * s)**(-1/2)
 
 #this is only needed in very hight (optical) frequencies, where epsilon and mu are complex
-def skinFull(s):
-    return (np.real(np.sqrt(1j * 10e6 * 12.57e-7 * 1 * (s + 1j * 1e6 * 8.85e-12))))**-1
+def skinFull(s, freq):
+    return (np.real(np.sqrt(1j * freq * 12.57e-7 * 1 * (s + 1j * 1e6 * 8.85e-12))))**-1
 
-sigArr = np.linspace(1, 1e6, 1000)
+sigArr = np.linspace(1e5, 1e8, 1000)
 plt.close('all')
+
+
+freqs = np.linspace(50*10**6, 300*10**6, 3)
+for freq in freqs:
+    plt.loglog(sigArr, skinFull(sigArr, freq), label = '%i' %(freq/10**6) + 'MHz')
 
 plt.xlabel('sigma (S/m)')
 plt.ylabel('skin depth (m)')
-plt.title('Skin depth as a function of conductivity at 10 mHz')
-plt.loglog(sigArr, skin(sigArr))
-
+plt.title('Skin depth as a function of conductivity')
+#plt.loglog(sigArr, skinFull(sigArr))
+plt.legend()
