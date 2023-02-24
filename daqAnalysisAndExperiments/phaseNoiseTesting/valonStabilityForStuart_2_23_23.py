12fcd61c8ea6ec11fa0f9a97fc74b5974e9039d6
josephmlev <joseph@Josephs-MacBook-Pro.local>
2023-02-23 21:40:49 -0800

    adds valon stability .py start

---
+++ daqAnalysisAndExperiments/phaseNoiseTesting/valonStabilityForStuart_2_23_23.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import scipy.signal as sig
+import scipy.stats as stat
+import matplotlib.pyplot as plt
+import allantools as at
+import pandas as pd
+%matplotlib widget
