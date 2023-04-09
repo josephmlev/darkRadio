04f1785f63ce448be1a1245c2beb0f7e4daebeaa
dradmin <dradmin@darkradio2.physics.ucdavis.edu>
2023-04-08 17:19:10 -0700

    Updates switchmeBabyOneMoreTime to look for available arduinos and not use hard coded port

--- scripts/switchMeBabyOneMoreTime.py
+++ scripts/switchMeBabyOneMoreTime.py
@@ -1,6 +1,45 @@
-import serial
+import serial 
+import serial.tools.list_ports
+import getpass
+import os
+import pyvisa
 
-ser = serial.Serial('/dev/ttyACM0', 115200)
+# Get the current username
+username = getpass.getuser()
+
+# Get a list of available serial ports
+ports = serial.tools.list_ports.comports()
+
+# Search for a specific keyword in the port description
+keyword = "arduino"
+matching_ports = [(p.device, p.description) for p in ports if keyword.lower() in p.description.lower()]
+
+# Print the list of matching ports
+if matching_ports:
+    print('Ports with "arduino" found:')
+    for port, desc in matching_ports:
+        print(f'{port} : {desc}')
+
+
+if len(matching_ports) > 1:
+    print("Multple arduinos. Manually enter name")
+    portName = input('>> ')
+if len(matching_ports) == 0:
+    print("No Arduinos found")
+    exit()
+    
+print('Enter a port name. Ex /dev/ttyACM1')
+print(f'Or press enter to use {desc} on {port}')
+usrInput = input('>> ') 
+if usrInput == "":
+    portName = port
+else:
+    portName = useInput
+	
+
+# Change the ownership of the serial port device file to the current user
+os.system(f'sudo chown {username}:{username} {portName}')
+ser = serial.Serial(portName, 115200)
 
 while True:
     dataIn = input('enter to toggle')
