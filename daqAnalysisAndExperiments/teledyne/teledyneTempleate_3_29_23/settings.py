c88fd5396b3645af17f646b82d0e14eca65211df
dradmin <dradmin@darkradio2.physics.ucdavis.edu>
2023-07-06 16:30:06 -0700

    permissions change on a few teledyne files

--- daqAnalysisAndExperiments/teledyne/teledyneTempleate_3_29_23/settings.py
+++ daqAnalysisAndExperiments/teledyne/teledyneTempleate_3_29_23/settings.py
@@ -10,12 +10,12 @@ NOF_CHANNELS                    = 2 #can't currently change. Need to load 1 ch f
 CH0_RECORD_LEN                  = int(2**24) 
 CH1_RECORD_LEN                  = CH0_RECORD_LEN #Different lengths not currently accepted
 
-NOF_BUFFERS_TO_RECEIVE          = 10000 #for each call of avgFFT()
-NOF_ACQUISITIONS_TO_TAKE        = 1100 #number of times to call avgFFT()
+NOF_BUFFERS_TO_RECEIVE          = 20 * 514 #for each call of avgFFT()
+NOF_ACQUISITIONS_TO_TAKE        = 1 #number of times to call avgFFT()
 
 NOF_GPU_BUFFERS                 = 2 #per channel. Crashes if not 2.
 
-INIT_SLEEP_TIME			= 10 #Time in seconds to sleep before starting anything
+INIT_SLEEP_TIME			= 0 #Time in seconds to sleep before starting anything
 
 ##########################################################
 # CLOCKING
@@ -24,14 +24,14 @@ ADQ_CLOCK_SOURCE                = 1 # 0 = INTERNAL, 1 = EXTERNAL
 				    # If external clock is used, it must be set up seperatly!!!
 				    # See valon_Init.py
 
-CH0_SAMPLE_SKIP_FACTOR          = 2  #For factors below 8, only power-of-two values are allowed 
+CH0_SAMPLE_SKIP_FACTOR          = 4  #For factors below 8, only power-of-two values are allowed 
 CH1_SAMPLE_SKIP_FACTOR          = CH0_SAMPLE_SKIP_FACTOR #Different values are untested
 
 # Must be 2.5e9 if ADQ_CLOCK_SOURCE==0
 # Else, must be between 1e9 and 2.5e9. 
 # Use sample skip to get lower than 1GHz
 # Should be a multple of 20MHz for best performance
-CLOCK_RATE                      = 1.32e9 #Hz
+CLOCK_RATE                      = 2.5e9 #Hz
 
 VALON_EXT_10MHZ                 = 1
 
@@ -46,16 +46,18 @@ PERIODIC_EVENT_SOURCE_PERIOD    = int(CH0_RECORD_LEN + 100)
 # Save a single spectrum for testing or simple DAQ stuff
 # NOF_ACQUISITIONS_TO_TAKE should be 1
 SAVE_W_SPEC                     = 0 #Saves last spec to .npy.
-PATH_TO_SAVE_SINGLE_SPEC        ='/drBigBoy/darkRadio/daqAnalysisAndExperiments/run1p3/rfInterferenceTesting/data_gitignore/W_dict_ultraParanoid_everythingoff_1MFFTs_3_26_23_v2' #Where to save above
+PATH_TO_SAVE_SINGLE_SPEC        ='/drBigBoy/darkRadio/daqAnalysisAndExperiments/run1p4/termSpec_run1p4AmpChainPlus5dBAttenuation_gitignore' #Where to save above
 
-NUM_SPEC_PER_FILE               = 16 #How many spectra to put in a file. Keep files around 1GB. 16 is good
-SAVE_DIRECTORY                  = '/drBiggerBoy/runTest_3_30_23/' #directory to save data. Note this needs to be created ahead of time
+NUM_SPEC_PER_FILE               = 25 #How many spectra to put in a file. Keep files around 1GB. 16 is good
+SAVE_DIRECTORY                  = '/drBiggerBoy/run1p4_termRun/' #directory to save data. Note this needs to be created ahead of time
                                                                 #and there should be a subdirectory called data. 
 
-SAVE_H5                         = 1 # Should h5 be saved AND database.txt be updated. 
+SAVE_H5                         = 0 # Should h5 be saved AND database.txt be updated. 
 
-SWITCH                          = 1 #Controlls if a switch should be used. Requires Arduino to be connected. Should configure automatically
-SWITCH_SLEEP_TIME		= .05 #Time in seconds to sleep after switching
+SWITCH                          = 0 #Controlls if a switch should be used. Requires Arduino to be connected. Should configure automatically
+SWITCH_SLEEP_TIME		= .5 #Time in seconds to sleep after switching
+
+SWITCH_DUTYCYCLE 		= 1 # Really 1/duty cycle. If loopNum % SWITCH_DUTYCYCLE == 0 then switch. Buggy, test if using.
 
 MANUAL_ACQNUM                   = -1 # Manually set acquisition number. Set to -1 to 
                                      # get from database. NOT TESTED
@@ -72,7 +74,7 @@ SETUP_DICT                      = { 'AMP1'          : 'PAS_1012',
                                     'PATCH PANNEL'  : 'YES',
                                     'HPF'           : '288S+',
                                     'LPF'           : 'HSP50+',
-                                    'ATTENUATOR'    : '13dB_FIXED', 
+                                    'ATTENUATOR'    : '4dB_FIXED', 
                                     'ADC'           : 'ADQ32',
                                     'CLOCK'         : 'SRS_VIA_VALON',
                                     }
@@ -83,7 +85,7 @@ PLOT_FINAL_SPEC			= 1 # Plots the last call from avgFFT
 VERBOSE_MODE                    = 0 # Print info
 PRINT_BUF_COUNT                 = 0 # Prints how many buffers have been collected
 TEST_MODE                       = 0 # Copy to cpu, ~100ms overhead
-CH_TO_TEST                      = 0 # Sets which channel to plot and save. assumes TEST_MODE = 1
+CH_TO_TEST                      = 1 # Sets which channel to plot and save. assumes TEST_MODE = 1
 SAVE_BUFFER                     = 0 # Sets if the buffer should be saved.   //      //      //
 PLOT_TIME_DOMAIN                = 0 # Plots time domain data                //      //      // 
 # Available test pattern signals:
@@ -151,7 +153,7 @@ if TEST_MODE == 0:
 if ADQ_CLOCK_SOURCE == 0 and CLOCK_RATE != 2.5E9:
     raise ValueError(f"Settings.py: ADQ_CLOCK_SOURCE is set to 0 (internal). This requires CLOCK_RATE = 2.5 GHz. 2.5GHz is not a strict limit (see ADQ32 manual) but this code is not set up to change it and requires 2.5GHz clock. You can change the sample rate by dividing the clock by a factor of 2 using settings.sample skip. If more flexible clocking is required, use an external clock.")
     
-if ADQ_CLOCK_SOURCE == 1 and not(1e9<CLOCK_RATE<2.5e9):
+if ADQ_CLOCK_SOURCE == 1 and not(1e9<CLOCK_RATE<=2.5e9):
     raise ValueError(f"Settings.py: ADQ_CLOCK_SOURCE is set to 1 (external) and CLOCK_RATE is set to {CLOCK_RATE/1E6} MHz. This requires 1<CLOCK_RATE<2.5GHz. See valonInit.py to set up the Valon, or change ADQ_CLOCK_SOURCE = 0 (internal) and set CLOCK_RATE = 2.5GHz")
     
 if (SAMPLE_RATE*BYTES_PER_SAMPLES*NOF_CHANNELS > 7e9 
