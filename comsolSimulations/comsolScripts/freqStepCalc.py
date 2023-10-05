f4c0efbd56ae4e784abe6d28143dd9140a101b19
dradmin <dradmin@darkradio2.physics.ucdavis.edu>
2023-10-04 18:41:26 -0700

    adds rounding to freq step calc

--- comsolSimulations/comsolScripts/freqStepCalc.py
+++ comsolSimulations/comsolScripts/freqStepCalc.py
@@ -1,13 +1,13 @@
-fStart  = 50
-fStop   = 400
+fStart  = 40
+fStop   = 310
 Q       = 500
 f       = fStart
 fList   = [f]
 while f < fStop:
     step = f/Q
-    fList.append(f+step)
+    fList.append(round(f+step,3))
     f   += step
 
-
+import numpy as np
 print(fList)
 print(len(fList))
